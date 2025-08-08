5d59e3dfc7805c42e7a1f5b4e67bdef8ea301bc5
GenesisMagno <genesisgmagno@gmail.com>
2025-08-08 20:29:25 +0800

    Update MainLayout.jsx

--- react-frontend/src/Layouts/MainLayout.jsx
+++ react-frontend/src/Layouts/MainLayout.jsx
@@ -37,7 +37,7 @@ export default function MainLayout() {
           {user ? (
             <div className="group relative">
               <img
-                src={`${import.meta.env.VITE_API_URL}/storage/${user?.image}` || `${import.meta.env.VITE_API_URL}/images/noimage.png`}
+                src={`${import.meta.env.VITE_API_URL}/storage/${user?.image}` || `${import.meta.env.VITE_API_URL}/public/images/noimage.png`}
                 alt="profile"
                 className="ml-2 h-10 w-10 rounded-full cursor-pointer"
               />
